2cdf2a7076a284c598f16ae72bfed14053278061
Lester <32542078+lhes23@users.noreply.github.com>
2023-03-10 21:13:17 +0800

    delete mongodb clientPromise

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,17 +1,3 @@
-// import clientPromise from "../../lib/mongodb"
-
-// export default async (req, res) => {
-//   try {
-//     const client = await clientPromise
-//     const db = client.db("bill_app_db")
-
-//     const users = await db.collection("users").find({}).toArray()
-//     res.json(users)
-//   } catch (error) {
-//     console.error(error)
-//   }
-// }
-
 import connectMongo from "../../util/connectMongo"
 import User from "../../models/user"
 
