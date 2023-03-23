cc8481587ffe6dac7d726fffdbc875a53be3351f
Lester <32542078+lhes23@users.noreply.github.com>
2023-03-23 19:27:52 +0800

    add appDetails

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
+
+export const appDetails = {
+  appName: "Billing App"
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
