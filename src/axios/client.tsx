a637c05ea8994b77718860b9255a90922a0dd923
Lester <32542078+lhes23@users.noreply.github.com>
2023-03-23 19:54:13 +0800

    add client for axios

--- src/axios/client.tsx
+++ src/axios/client.tsx
@@ -1,7 +1,8 @@
 import axios, { AxiosInstance } from "axios"
 
 const client: AxiosInstance = axios.create({
-  baseURL: process.env.NODE_ENV === "development" ? "http://localhost:3000" : ""
+  baseURL:
+    process.env.NODE_ENV === "development" ? "http://192.168.100.30:3000" : ""
 })
 
 export default client
