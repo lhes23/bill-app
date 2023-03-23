1f025d6da923364ede211ca032978c70a8d5c066
Lester <32542078+lhes23@users.noreply.github.com>
2023-03-23 21:34:00 +0800

    add dummy data - houses

--- src/components/dashboard/HouseCard.tsx
+++ src/components/dashboard/HouseCard.tsx
@@ -1,23 +1,50 @@
 import React from "react"
 import { FaHouseUser } from "react-icons/fa"
 
+const houses = [
+  {
+    id: "1",
+    name: "House A",
+    status: true
+  },
+  {
+    id: "2",
+    name: "House B",
+    status: true
+  },
+  {
+    id: "3",
+    name: "House C",
+    status: true
+  },
+  {
+    id: "4",
+    name: "House D",
+    status: true
+  }
+]
+
 const HouseCard = () => {
   return (
     <>
-      <div className="card w-3/4 lg:w-96 glass">
-        <figure>
-          <img
-            src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            alt="car!"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Life hack</h2>
-          <p>How to park your car at your garage?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Learn now!</button>
+      <div className="flex p-20">
+        {houses.map((house) => (
+          <div key={house.id} className="card w-3/4 lg:w-96 glass mx-2">
+            <figure>
+              <img
+                src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt="car!"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{house.name}</h2>
+              <p>How to park your car at your garage?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Learn now!</button>
+              </div>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </>
   )
