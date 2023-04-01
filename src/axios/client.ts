2f3d8e50904378a33f17f679277fdc23195ceece
Lester <32542078+lhes23@users.noreply.github.com>
2023-04-01 21:02:41 +0800

    change base url

--- src/axios/client.ts
+++ src/axios/client.ts
@@ -2,7 +2,9 @@ import axios, { AxiosInstance } from "axios"
 
 const client: AxiosInstance = axios.create({
   baseURL:
-    process.env.NODE_ENV === "development" ? "http://192.168.100.30:3000" : ""
+    process.env.NODE_ENV == "development"
+      ? process.env.NEXT_PUBLIC_BASE_URL
+      : process.env.NEXT_PUBLIC_BASE_URL
 })
 
 export default client
