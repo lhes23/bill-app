76a10b9dcbf4ab14deb115366b7cbee0d6d15dd7
Lester <32542078+lhes23@users.noreply.github.com>
2023-04-03 07:40:10 +0800

    add key

--- src/components/dashboard/HouseCard.tsx
+++ src/components/dashboard/HouseCard.tsx
@@ -7,23 +7,21 @@ const HouseCard = ({ houses }: IHouses) => {
     <>
       <div className="grid grid-cols-2 lg:grid-cols-4 pt-4 justify-evenly text-center">
         {houses.map((house) => (
-          <>
-            <div key={house._id} className="card glass mx-4 my-4 z-0">
-              <figure>
-                <img
-                  src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-                  alt="car!"
-                />
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title">
-                  <FaHouseUser />
-                  {house.name}
-                </h2>
-                <p>{house.occupied ? "Occupied" : "Vacant"}</p>
-              </div>
+          <div key={house._id} className="card glass mx-4 my-4 z-0">
+            <figure>
+              <img
+                src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt="car!"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">
+                <FaHouseUser />
+                {house.name}
+              </h2>
+              <p>{house.occupied ? "Occupied" : "Vacant"}</p>
             </div>
-          </>
+          </div>
         ))}
       </div>
     </>
