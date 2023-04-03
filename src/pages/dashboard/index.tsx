0bad363a625af95c45aad2c0ab28d5ea0234f1f9
Lester <32542078+lhes23@users.noreply.github.com>
2023-04-03 15:25:59 +0800

    fix chart spacing

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -46,18 +46,22 @@ const Dashboard = ({ houses, activeTenants }: IProps) => {
           <HouseCard houses={houses} />
         </section>
         <section className="grid grid-cols-1 lg:grid-cols-2">
-          <AreaChart
-            datasets={datasets}
-            label="Electric"
-            color="green"
-            fillColor="rgba(23,23,12,0.6)"
-          />
-          <AreaChart
-            datasets={datasets}
-            label="Water"
-            color="blue"
-            fillColor="rgba(23,23,12,0.6)"
-          />
+          <div className="m-1">
+            <AreaChart
+              datasets={datasets}
+              label="Electric"
+              color="green"
+              fillColor="rgba(23,23,12,0.6)"
+            />
+          </div>
+          <div className="m-1">
+            <AreaChart
+              datasets={datasets}
+              label="Water"
+              color="blue"
+              fillColor="rgba(23,23,12,0.6)"
+            />
+          </div>
         </section>
       </PageLayout>
     </>
