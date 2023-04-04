98f283923de34714adbde80060351cc85df99f31
Lester <32542078+lhes23@users.noreply.github.com>
2023-04-04 08:32:32 +0800

    remove img avatar and spacing for the action buttons

--- src/components/dashboard/ActiveReadings.tsx
+++ src/components/dashboard/ActiveReadings.tsx
@@ -28,7 +28,7 @@ const ActiveReadings = () => {
       <h3 className="my-6 text-xl font-semibold">Active Readings</h3>
       <div className="w-full overflow-hidden rounded-lg shadow-xs">
         <div className="w-full overflow-x-auto">
-          <table className="w-full whitespace-no-wrap">
+          <table className="w-full whitespace-no-wrap table-auto">
             <thead>
               <tr className="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50">
                 <th className="px-4 py-3">Name of Tenant</th>
@@ -43,20 +43,8 @@ const ActiveReadings = () => {
             <tbody className="bg-white divide-y">
               {activeReadings.map((activeReading) => (
                 <tr className="" key={activeReading._id}>
-                  <td className="px-4 py-3">
+                  <td className="">
                     <div className="items-center text-sm">
-                      {/* Avatar with inset shadow */}
-                      <div className="relative hidden w-8 h-8 mr-3 rounded-full md:block">
-                        <img
-                          className="object-cover w-full h-full rounded-full"
-                          src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
-                          alt=""
-                        />
-                        <div
-                          className="absolute inset-0 rounded-full shadow-inner"
-                          aria-hidden="true"
-                        />
-                      </div>
                       <div>
                         <p className="font-semibold">
                           {
@@ -75,30 +63,28 @@ const ActiveReadings = () => {
                       </div>
                     </div>
                   </td>
-                  <td className="px-4 py-3 text-sm">
-                    {activeReading.billType}
-                  </td>
+                  <td className="text-sm">{activeReading.billType}</td>
                   <td className="text-xs">
                     <span className="px-2 py-1 font-semibold leading-tight  text-green-700 bg-green-100 rounded-full">
                       ₱ {activeReading.bill}
                     </span>
                   </td>
-                  <td className="px-4 py-3 text-sm">{activeReading.dueDate}</td>
-                  <td className="px-4 py-3 text-sm">
-                    <button
-                      className="btn btn-outline btn-warning"
-                      onClick={() => {}}
-                    >
-                      Edit
-                    </button>
-                    {/* </td>
-                  <td className="px-4 py-3 text-sm"> */}
-                    <button
-                      className="btn btn-outline btn-error"
-                      onClick={() => paidHandler(activeReading._id)}
-                    >
-                      Paid
-                    </button>
+                  <td className="text-sm">{activeReading.dueDate}</td>
+                  <td className="text-sm text-center">
+                    <div className="grid grid-cols-2 gap-1">
+                      <button
+                        className="btn btn-outline btn-warning p-1"
+                        onClick={() => {}}
+                      >
+                        Edit
+                      </button>
+                      <button
+                        className="btn btn-outline btn-error p-1"
+                        onClick={() => paidHandler(activeReading._id)}
+                      >
+                        Paid
+                      </button>
+                    </div>
                   </td>
                 </tr>
               ))}
